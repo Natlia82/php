d5260e114bd411a0af84d617f90bc40ffd76bf36
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2019-04-15 20:37:26 +0500

    Add files via upload

--- controller/Controller.php
+++ controller/Controller.php
@@ -3,198 +3,180 @@ session_start();
 include 'model/model.php';
 class Controller
 {
-    /**
-     * Получение всех книг
-     */
-    public function getList()
-    {  if (isset($_POST['but'])) {
-        if ($_SESSION['getList']) {
-         unset($_SESSION['getList']);
-        }
-         else {
-          $_SESSION['getList'] = true;
-         }
-         }
-        if (isset($_POST['butcancel'])) {
-          unset($_SESSION['getList']);
-        }
-
-        if (isset($_POST['butinsert'])) {
-          if (empty($_POST['text']) and empty($_POST['textarea'])) {
-       ///
-     } else {
-              $book = new Book();
-              $books = $book->insv($_POST['text'], $_POST['textarea'], $_POST['select']);
-              unset($_SESSION['getList']);
-         }
-
-        }
-
-        $book = new Book();
-        $books = $book->findAll();
-        $question = $book -> findAllQuestion();
-        Di::get()->render('model/list.php', ['books' => $books, 'question' => $question]);
+  /**
+  * главная страница пользователя
+  */
+  public function getList()
+  {   $topic = new Topic();
+    //добавление нового вопроса
+    if (isset($_POST['but'])) {
+      if ($_SESSION['getList']) {
+        unset($_SESSION['getList']);
+      }
+      else {
+        $_SESSION['getList'] = true;
+      }
     }
-    /**
-    *Ввод пароля администратор
-    **/
-    public function admin() {
-        if (isset($_POST['log']) and isset($_POST['pass'])) {
-        $log = $_POST['log'];
-        $pas = $_POST['pass'];
-        $admin = new Book();
-        $admin = $admin -> adminInput($log, $pas);
+    if (isset($_POST['butcancel'])) {
+      unset($_SESSION['getList']);
+    }
+    // при нажатие на кнопку сохранить добавляем вопрос
+    if (isset($_POST['butinsert'])) {
+      if (empty($_POST['text']) and empty($_POST['textarea'])) {
+      } else {
+        $insertTopic = $topic->insv($_POST['text'], $_POST['textarea'], $_POST['select']);
+        unset($_SESSION['getList']);
+      }
+    }
+    //получение всех тем
+    $allTopics = $topic->findAll();
+    //получение всех вопросов
+    $question = $topic -> findAllQuestion();
+    Di::get()->render('model/list.php', ['allTopics' => $allTopics, 'question' => $question]);
+  }
+  /**
+  *Ввод пароля администратор
+  **/
+  public function admin() {
+    if (isset($_POST['log']) and isset($_POST['pass'])) {
+      $log = $_POST['log'];
+      $pas = $_POST['pass'];
+      $admin = new Topic();
+      $admin = $admin -> adminInput($log, $pas);
 
-       if (!empty($admin)) {
+      if (!empty($admin)) {
         $_SESSION['variable'] = $admin[0][0];
-         header('Location: /index.php?c=book&a=adminlist');
+        header('Location: /index.php?c=topic&a=adminlist');
 
-    } elseif ($log == 'admin' and $pas == 'admin') {
-       $_SESSION['variable'] = "true";
-       header('Location: /index.php?c=book&a=adminlist');
-     } else  unset($_SESSION['variable']);
+      } elseif ($log == 'admin' and $pas == 'admin') {
+        $_SESSION['variable'] = "true";
+        header('Location: /index.php?c=topic&a=adminlist');
+      } else  unset($_SESSION['variable']);
+    }
+    Di::get()->render('model/admin.php');
+  }
+  /**
+  *главная страница для администратора
+  **/
+  public function adminlist($param) {
+    // если нет пароля (сессия) то на стартовую страницу
+    if (empty($_SESSION['variable'])) {
+      header('Location: /');
+    } else {
+      $topic = new Topic();
+      //добавить администратора при нажатие открываются поля для ввода при повторном нажатие поля не видны
+      if (isset($_POST["newadmin"])) {
+        if (!$_SESSION["newadmin"]) {
+          $_SESSION["newadmin"] = 'true';
+        } else {
+          unset($_SESSION["newadmin"]);
+        }
+      }
+      //сохраняем нового администратора
+      if (isset($_POST["savadmin"])) {
+        $ins = $topic-> addadmin($_POST["login"], $_POST["passw"]);
+        unset($_SESSION["new"]);
+      }
+      //удаление админив
+      if ($param !== null) {
+        $del = $topic -> delAdmin($param);
+      }
+      //добавить новую тему при нажатие открываются поля для ввода
+      if (isset($_POST["new"])) {
+        if (!$_SESSION["new"]) {
+          $_SESSION["new"] = 'true';
+        } else {
+          unset($_SESSION["new"]);
+        }
+      }
+      // отменить добавление темы
+      if (isset($_POST["cansel"])) {
+        unset($_SESSION["new"]);
+      }
+      //сохраняем новую тему
+      if (isset($_POST["save"])) {
+        $ins = $topic-> add($_POST["newtema"]);
+        unset($_SESSION["new"]);
+      }
+      //кнопка удалить тему , открывается доп меню для удаления
+      if (isset($_POST["delet"])) {
+        if (!$_SESSION["delet"]) {
+          $_SESSION["delet"] = 'true';
+        } else {
+          unset($_SESSION["delet"]);
+        }
+      }
+      // отменить удаления
+      if (isset($_POST["cansel2"])) {
+        unset($_SESSION["delet"]);
+      }
+      //удаление темы
+      if (isset($_POST["del"])) {
+        $del = $topic -> del($_POST["option"]);
+        unset($_SESSION["delet"]);
       }
 
-      Di::get()->render('model/admin.php');
-    }
-
-    /**
-    *Вxод администраторa
-    **/
-
-   public function adminlist($param) {
-  //   echo "привет";
-    if (empty($_SESSION['variable'])) {
-        header('Location: /');
-    } else {
-    //   echo "controller";
-            $book = new Book();
-            if (isset($_POST["new"])) {
-              if (!$_SESSION["new"]) {
-                $_SESSION["new"] = 'true';
-              } else {
-                       unset($_SESSION["new"]);
-                     }
-            }
-            if (isset($_POST["delet"])) {
-              if (!$_SESSION["delet"]) {
-                $_SESSION["delet"] = 'true';
-              } else {
-                       unset($_SESSION["delet"]);
-                     }
-            }
-            if (isset($_POST["cansel2"])) {
-               unset($_SESSION["delet"]);
-            }
-
-            if (isset($_POST["del"])) {
-               $del = $book -> del($_POST["option"]);
-               unset($_SESSION["delet"]);
-            }
-            if (isset($_POST["cansel"])) {
-               unset($_SESSION["new"]);
-            }
-
-            if (isset($_POST["save"])) {
-            //  $sql = "INSERT INTO `topic`(`mane`) VALUES ('".$_POST["newtema"]."')";
-            //  $dbcon->exec($sql);
-               $ins = $book-> add($_POST["newtema"]);
-               unset($_SESSION["new"]);
-            }
-
-         $books = $book->findAll();
-         $question = $book -> findAllQuestion();
-         $administrator = $book -> findAdmin();
-         if ($param !== null) {
-           $del = $book -> delAdmin($param);
-         }
-
-         if (isset($_POST["newadmin"])) {
-           if (!$_SESSION["newadmin"]) {
-             $_SESSION["newadmin"] = 'true';
-           } else {
-                    unset($_SESSION["newadmin"]);
-                  }
-         }
-
-         if (isset($_POST["savadmin"])) {
-            $ins = $book-> addadmin($_POST["login"], $_POST["passw"]);
-            unset($_SESSION["new"]);
-         }
-
-        //   echo "мы тут";
-        if ($_POST["checkbox"] == on) {
+      //массив всех тем
+      $topics = $topic->findAll();
+      //массив всех вопросов
+      $question = $topic -> findAllQuestion();
+      //массив всех админов
+      $administrator = $topic -> findAdmin();
+      //если выбран чекбокс показывать без ответа
+      if ($_POST["checkbox"] == on) {
         //  echo "мы и тут";
-          $_SESSION["checkbox"] = "checked='checke'";
+        $_SESSION["checkbox"] = "checked='checke'";
+        // массив всех вопросов без ответа отсортированных по дате
+        $question = $topic -> findAllQuestion2();
+      } else {
+        $_SESSION["checkbox"] = "";
+        $question = $topic -> findAllQuestion();
+      }
+      Di::get()->render('model/adminlist.php', ['topics' => $topics, 'question' => $question, 'administrator' => $administrator ]);
+    }
+  }
 
-          $question = $book -> findAllQuestion2();
-          } else {
-                   $_SESSION["checkbox"] = "";
-                //   echo "и сдесь";
+  /**
+  * Редактирование вопроса
+  */
+  public function update($params)
+  {
+    $topic = new Topic();
+    //отмена перенаправляем на страницу администратора
+    if (isset($_POST['submit2'])) {
+      header('Location: /index.php?c=topic&a=adminlist');
+    }
+    // удаляем вопрос и перенаправляем на страницу администратора
+    if (isset($_POST['submit'])) {
+      $delete = $topic -> deleteQuestion($params);
+      header('Location: /index.php?c=topic&a=adminlist');
+    }
+    // сохраняем изменения и перенаправляем на страницу администратора
 
-                   $question = $book -> findAllQuestion();
-                 }
+    if (isset($_POST["action"])) {
 
+      if ($_POST["checkbox"] == on) {
+        $status = 1;
+      } else {
+        $status = 0;
+      }
 
-         Di::get()->render('model/adminlist.php', ['books' => $books, 'question' => $question, 'administrator' => $administrator ]);
-
+      if ($_POST["option"]) {
+        $option = $_POST["option"];
+      } else $option = "";
+      //сохраняем изменения
+      $ins = $topic-> updateQuestion($_POST["textarea"], $_POST["textarea2"], $status, $option, $_POST["autor"], $params);
+      header('Location: /index.php?c=topic&a=adminlist');
     }
 
-   }
+    //массив с информацией о вопросе
+    $question = $topic->find($params);
+    //массив всех тем
+    $topics = $topic->findAll();
+    Di::get()->render('model/update.php', ['question' => $question, 'topics' => $topics]);
 
-   /**
-    * Редактирование данных
-    */
-    public function update($params)
-   {
-               $book = new Book();
-               $books = $book->find($params);
-              echo "</br>"."тутик ".$books;
-             Di::get()->render('model/update.php', ['result' => $result]);
-
-   }
-
-	/**
-	 *
-	 */
-/*	function add()
-	{
-        $book = new Book();
-	    $errors = [];
-        if (count($_POST) > 0) {
-            $data = [];
-            if (isset($_POST['name']) && preg_match('/[A-zА-я\s]+/', $_POST['name'])) {
-                $data['name'] = $_POST['name'];
-            } else {
-                $errors['name'] = 'Error name';
-            }
-            if (isset($_POST['author']) && preg_match('/[A-zА-я\s]+/', $_POST['author'])) {
-                $data['author'] = $_POST['author'];
-            } else {
-                $errors['author'] = 'Error author';
-            }
-            if (isset($_POST['year']) && is_numeric($_POST['year'])) {
-                $data['year'] = $_POST['year'];
-            } else {
-                $errors['year'] = 'Error year';
-            }
-            if (isset($_POST['genre']) && preg_match('/[A-zА-я\s]+/', $_POST['author'])) {
-                $data['genre'] = $_POST['genre'];
-            } else {
-                $errors['genre'] = 'Error genre';
-            }
-            if (count($errors) == 0) {
-                $idAdd = $book->add($data);
-                if ($idAdd) {
-                    header('Location: /');
-                }
-            }
-        }
-        Di::get()->render('book/add.php');
-    }
-
+  }
 
 }
-*/ }
 
 ?>
