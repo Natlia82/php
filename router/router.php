4191d9a7bcf66e26f1ff5d9b4544a282667bc059
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2019-04-15 20:35:18 +0500

    Add files via upload

--- router/router.php
+++ router/router.php
@@ -1,46 +1,38 @@
 <?php
-/**
- * Первый уровень понимания роутеров.
- * Намерено сделано через if и else
- * Пример урла: /?c={controller}&a={action}&{param1}={value1}&{param2}={value2}
- * /?c=book&a=update&id=1
-  /?c=book&a=add
- */
 if (! isset($_GET['c']) || ! isset($_GET['a'])) {
-    $controller = 'book';
-    $action = 'list';
+  $controller = 'topic';
+  $action = 'list';
 } else {
-    $controller = $_GET['c'];
-    $action = $_GET['a'];
+  $controller = $_GET['c'];
+  $action = $_GET['a'];
 }
-if ($controller == 'book') {
-    include 'controller/Controller.php';
-    $Controller = new Controller();
-    if ($action == 'list') {
-        $Controller->getList();
-        //controllerList();
+if ($controller == 'topic') {
+  include 'controller/Controller.php';
+  $Controller = new Controller();
+  //стартавая страница пользователя
+  if ($action == 'list') {
+    $Controller->getList();
+  }
+  //страница для ввода пароля
+  elseif ($action == 'admin') {
+    $Controller->admin();
+  }
+
+  //главная страница администратора
+  elseif ($action == 'adminlist') {
+    if (isset($_GET['id'])) {
+      $param = $_GET['id'];
+    } else {
+      $param = null;
     }
+    $Controller->adminlist($param);
+  }
+  
+  //редактирование вопроса
+  elseif ($action == 'update') {
+    $Controller->update($_GET['id']);
+  }
 
-   elseif ($action == 'admin') {
-      $Controller->admin();
+}
 
-   }
-   elseif ($action == 'adminlist') {
-  //   echo "router";
-      if (isset($_GET['id'])) {
-        $param = $_GET['id'];
-      } else {
-        $param = null;
-      }
-      $Controller->adminlist($param);
-   }
-   elseif ($action == 'update') {
-     echo "router".$_GET['id'];
-         $Controller->update($_GET['id']);
-       //controllerUpdate();
-     }
-
-   } 
-
-
- ?>
+?>
