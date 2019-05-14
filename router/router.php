a4aae17933ec44c60566ca31dd330d8d7abc21c3
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2019-05-14 16:36:12 +0500

    Add files via upload

--- router/router.php
+++ router/router.php
@@ -1,37 +1,85 @@
 <?php
 if (! isset($_GET['c']) || ! isset($_GET['a'])) {
-  $controller = 'topic';
-  $action = 'list';
+    $controller = 'topic';
+    $action = 'list';
 } else {
-  $controller = $_GET['c'];
-  $action = $_GET['a'];
+    $controller = $_GET['c'];
+    $action = $_GET['a'];
 }
-if ($controller == 'topic') {
-  include 'controller/Controller.php';
-  $Controller = new Controller();
-  //стартавая страница пользователя
-  if ($action == 'list') {
-    $Controller->getList();
-  }
-  //страница для ввода пароля
-  elseif ($action == 'admin') {
-    $Controller->admin();
-  }
 
-  //главная страница администратора
-  elseif ($action == 'adminlist') {
-    if (isset($_GET['id'])) {
-      $param = $_GET['id'];
-    } else {
-      $param = null;
+// страница пользователя
+if ($controller == 'topic') {
+    include 'controller/Controller.php';
+    $controller = new Controller();
+
+    //стартавая страница пользователя
+    if ($action == 'list') {
+        $controller->getList();
     }
-    $Controller->adminlist($param);
-  }
-  
-  //редактирование вопроса
-  elseif ($action == 'update') {
-    $Controller->update($_GET['id']);
-  }
+    //добавление вопроса
+    elseif ($action == 'questionAdd') {
+        $controller->questionAdd();
+    }
+}
+
+
+//страница для ввода пароля
+elseif ($controller == 'admin') {
+    include 'controller/ControllerAdmin.php';
+    $controllerAdmin = new ControllerAdmin();
+    if (isset($_GET['b'])) {
+        $activity = $_GET['b'];
+    }
+    else {
+        $activity = null;
+    }
+    if ($action == 'pass')  {
+        $controllerAdmin->admin();
+    }
+
+
+    //главная страница администратора
+    elseif ($action == 'adminlist') {
+        if (isset($_GET['id'])) {
+            $param = $_GET['id'];
+        } else {
+            $param = null;
+        }
+        // стартовая страница администратора
+        if ($activity == null) {
+            $controllerAdmin->adminlist();
+        }
+        // удаление администратора
+        elseif ($activity == 'deladm') {
+            $controllerAdmin->admindel($param);
+        }
+        //добавление админситраора
+        elseif  ($activity == 'adminadd') {
+            $controllerAdmin->adminadd();
+        }
+        //изменяем администратора
+        elseif ($activity == 'updadm') {
+            $controllerAdmin->updadm($param);
+        }
+        //добавляем новую тему
+        elseif ($activity == 'topicAdd') {
+            $controllerAdmin->topicAdd();
+        }
+        //удаление темы
+        elseif ($activity == 'topicDell') {
+            $controllerAdmin->topicDell();
+        }
+
+    }
+    
+    //редактирование вопроса
+    elseif ($action == 'update') {
+        $controllerAdmin->update($_GET['id']);
+    }
+    //удаление вопроса
+    elseif ($action == 'delet') {
+        $controllerAdmin->delet($_GET['id']);
+    }
 
 }
 
